025491160165078a0184370c66ed75546222c6a5
Aaron <aaturner1995@gmail.com>
2023-02-04 18:31:36 -0400

    typo in script file

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -66,7 +66,7 @@ nextQuestion = () => {
                 alert('Correct');
             } else {
                 alert('Incorrect');
-                
+            }
             nextQuestion();
         });
     });
