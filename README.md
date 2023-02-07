2067cd6f366151c613f859e9be40e901a6b1b925
Aaron <aaturner1995@gmail.com>
2023-02-07 09:43:26 -0400

    added README

---
+++ README.md
@@ -0,0 +1,20 @@
+# Javascript, HTML and CSS Coding Quiz
+
+## Description
+
+A coding quiz that uses HTML, CSS and Javascript to dynamically update the page on button clicks and answer selections. Highscores are stored locally.
+
+Each correct answer is worth 10 points and each incorrect answer results in a time penalty of 10 seconds.
+
+This repo contains the HTML, CSS, and Javascript to create this website.
+
+## Remote Site
+
+The remote site is hosted on GitHub Pages
+
+##### The remote site is located here:
+https://aturner1995.github.io/coding-quiz/
+
+## Screenshot
+
+
---
+++ assets/JS/questionBank.js
@@ -0,0 +1,107 @@
+var questions = [
+    {
+        question: "Which is not valid data type in Javascript?",
+        choice1: 'Undefined',
+        choice2: 'Boolean',
+        choice3: 'Float',
+        choice4: 'Number',
+        answer: 'Float'
+    },
+    {
+        question: "Which tag is used to write the javascript code?",
+        choice1: '<script>',
+        choice2: '<sp>',
+        choice3: '<javascript>',
+        choice4: '<java>',
+        answer: '<script>'
+    },
+    {
+        question: "What is the output: var a = '20'; var b = a = 30; console.log(a+b)",
+        choice1: '20',
+        choice2: '60',
+        choice3: '30',
+        choice4: '50',
+        answer: '60'
+    },
+    {
+        question: "Types of errors in Javascript",
+        choice1: 'Run time errors',
+        choice2: 'Compile Time Erroe',
+        choice3: 'Logical Error',
+        choice4: 'None of the Above',
+        answer: 'Run time errors'
+    },
+    
+    {
+        question: "Which property is used to set the background color of an HTML element?",
+        choice1: 'background-color',
+        choice2: 'bgcolor',
+        choice3: 'color',
+        choice4: 'backgroundColor',
+        answer: 'background-color'
+        },
+        {
+        question: "What is the default value of the position property in CSS?",
+        choice1: 'fixed',
+        choice2: 'static',
+        choice3: 'relative',
+        choice4: 'absolute',
+        answer: 'static'
+        },
+        {
+        question: "What is the CSS syntax to change the text color of an HTML element?",
+        choice1: 'color: red;',
+        choice2: 'text-color: red;',
+        choice3: 'font-color: red;',
+        choice4: 'textColor: red;',
+        answer: 'color: red;'
+        },
+        {
+        question: "What does HTML stand for?",
+        choice1: 'Hyper Text Markup Language',
+        choice2: 'Hyperlinks and Text Markup Language',
+        choice3: 'Home Tool Markup Language',
+        choice4: 'Hyper Text Programming Language',
+        answer: 'Hyper Text Markup Language'
+        },
+        {
+        question: "What is the CSS property used to change the font of an HTML element?",
+        choice1: 'font-family',
+        choice2: 'font-type',
+        choice3: 'type-font',
+        choice4: 'fontFamily',
+        answer: 'font-family'
+        },
+        {
+        question: "Which CSS property is used to set the width and height of an HTML element?",
+        choice1: 'width-height',
+        choice2: 'height-width',
+        choice3: 'dimension',
+        choice4: 'size',
+        answer: 'dimension'
+        },
+        {
+        question: "What is the JavaScript method used to add a new element to an HTML document?",
+        choice1: 'addElement()',
+        choice2: 'appendChild()',
+        choice3: 'append()',
+        choice4: 'createElement()',
+        answer: 'createElement()'
+        },
+        {
+        question: "What is the JavaScript syntax for creating a function?",
+        choice1: 'function = myFunction()',
+        choice2: 'function:myFunction()',
+        choice3: 'function myFunction()',
+        choice4: 'myFunction = function()',
+        answer: 'function myFunction()'
+        },
+        {
+        question: "What is the default value of display property in CSS?",
+        choice1: 'none',
+        choice2: 'block',
+        choice3: 'inline',
+        choice4: 'inline-block',
+        answer: 'inline'
+        }
+];
